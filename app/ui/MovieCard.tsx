43eaf1177a890513a4c2f6ba2c6ef5789d379957
mchtkk <mucahit.kuk@gmail.com>
2023-11-10 20:48:51 +0300

    qqqq

--- app/ui/MovieCard.tsx
+++ app/ui/MovieCard.tsx
@@ -7,7 +7,7 @@ const MovieCard: FC<MovieCardProps> = ({ title, overview, genre_ids, vote_averag
   const imageUrl = `https://image.tmdb.org/t/p/w370_and_h556_multi_faces${backdrop_path}`;
 
   return (
-    <div className="relative grid h-[32rem] w-full max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+    <div className="relative grid  w-full max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
       <div
         className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://image.tmdb.org/t/p/w370_and_h556_multi_faces/tM7uHa2Km5gSakooTQsZLwit3PK.jpg')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none"
         style={{
@@ -16,9 +16,9 @@ const MovieCard: FC<MovieCardProps> = ({ title, overview, genre_ids, vote_averag
           backgroundPosition: "center",
         }}
       >
-        <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50 backdrop-blur-[1px]"></div>
+        <div className="absolute inset-0  to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50 backdrop-blur-[1px]"></div>
       </div>
-      <div className="relative max-h-10 p-6 px-6 py-14 md:px-12">
+      <div className="relative p-6 px-6 py-14 md:px-12">
         <h2 className="mb-2 block font-sans text-3xl font-medium leading-[1.5] tracking-normal text-white antialiased ">
           {title}
         </h2>
@@ -26,8 +26,8 @@ const MovieCard: FC<MovieCardProps> = ({ title, overview, genre_ids, vote_averag
           {genre_ids}
         </h4>
         <h5 className="text-white font-bold line-clamp-2">IMDB: {vote_average}</h5>
-        <div className= "min-h-full mt-1">
-          <p style={{ overflow: "auto",height: "15rem"}} className="text-white">{overview}</p>
+        <div className= "mt-1">
+          <p className="text-white">{overview}</p>
         </div>
       </div>
     </div>
