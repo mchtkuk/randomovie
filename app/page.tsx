adca55b871adc4bd5499fc0d6f2cf2e190bda324
mchtkk <mucahit.kuk@gmail.com>
2023-12-12 14:46:58 +0300

    loading state added for random

--- app/page.tsx
+++ app/page.tsx
@@ -6,39 +6,42 @@ import { GenreIds, MovieItem, genresById } from "./lib/definition";
 import MovieCard from "./ui/MovieCard";
 import Featured from "./ui/Featured";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination } from "swiper/modules";
+import { ThreeDots } from 'react-loading-icons'
 import "swiper/swiper-bundle.css";
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 export default function Home() {
   const [randomMovie, setRandomMovie] = useState<MovieItem>();
   const [featuredMovie, setFeaturedMovie] = useState<MovieItem>();
   const [featuredTv, setFeaturedTv] = useState<MovieItem>();
+  const [loading, setLoading] = useState(false);
 
   const handleGenerateMovie = () => {
+    setLoading(true);
     getRandomMovie().then((movie) => {
       setRandomMovie(movie);
+      setLoading(false);
     });
   };
 
-  const featuredGenerateMovie = () => {
+  async function FetchData() {
+    await getRandomMovie().then((movie) => {
+      setRandomMovie(movie);
+      setLoading(false);
+    });
     getFeaturedMovie().then((featured) => {
       setFeaturedMovie(featured);
     });
-  };
-
-  const featuredGenerateTv = () => {
     getFeaturedTv().then((featured) => {
-      setFeaturedTv(featured)
-    })
+      setFeaturedTv(featured);
+    });
   }
 
   useEffect(() => {
-    handleGenerateMovie();
-    featuredGenerateMovie();
-    featuredGenerateTv();
+    FetchData()
   }, []);
 
   const mapGenreIdsToNames = (genreIds: GenreIds[]): string[] => {
@@ -58,19 +61,23 @@ export default function Home() {
           RandoMovie
         </button>
       </header>
-      <section className="w-full relative flex justify-center items-center">
-        {randomMovie && (
-          <MovieCard
-            title={randomMovie.title}
-            overview={randomMovie.overview}
-            genre_ids={genreNames.join(" ")}
-            vote_average={randomMovie.vote_average}
-            backdrop_path={randomMovie.backdrop_path}
-            release_date={randomMovie.release_date}
-            original_language={randomMovie.original_language}
-            popularity={randomMovie.popularity}
-            onClick={handleGenerateMovie}
-          />
+      <section className="w-full relative flex justify-center min-h-screen items-center">
+        {loading ? (
+          <ThreeDots />
+        ) : (
+          randomMovie && (
+            <MovieCard
+              title={randomMovie.title}
+              overview={randomMovie.overview}
+              genre_ids={genreNames.join(" ")}
+              vote_average={randomMovie.vote_average}
+              backdrop_path={randomMovie.backdrop_path}
+              release_date={randomMovie.release_date}
+              original_language={randomMovie.original_language}
+              popularity={randomMovie.popularity}
+              onClick={handleGenerateMovie}
+            />
+          )
         )}
       </section>
       <div className="text-center">
@@ -78,28 +85,28 @@ export default function Home() {
       </div>
       <section className="flex flex-row justify-center items-center mb-6 ">
         <Swiper
-        slidesPerView={2}
-        spaceBetween={10}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-          },
-          1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          1080: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          }
-        }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper"
+          slidesPerView={2}
+          spaceBetween={10}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1080: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
         >
           {featuredMovie &&
             featuredMovie.map((featured) => (
@@ -119,28 +126,28 @@ export default function Home() {
       </div>
       <section className="flex flex-row justify-center items-center mb-6 ">
         <Swiper
-        slidesPerView={2}
-        spaceBetween={10}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-          },
-          1024: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          1080: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          }
-        }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper"
+          slidesPerView={2}
+          spaceBetween={10}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            1080: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
         >
           {featuredTv &&
             featuredTv.map((featured) => (
